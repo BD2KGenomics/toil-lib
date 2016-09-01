87b18fe85da1d78f4f10d2b9c503f43fd46b6ef4
John Vivian <jtvivian@gmail.com>
2016-09-01 12:07:41 -0700

    Initial setup of Toil-lib repo

---
+++ setup.py
@@ -0,0 +1,36 @@
+from version import version
+from setuptools import find_packages, setup
+
+
+toil_version = '3.3.1'
+s3am_version = '2.0'
+
+
+kwargs = dict(
+    name='toil-lib',
+    version=version,
+    description='A common library for functions and tools used in toil-based pipelines',
+    author='UCSC Computational Genomics Lab',
+    author_email='cgl-toil@googlegroups.com',
+    url="https://github.com/BD2KGenomics/toil-lib",
+    install_requires=[
+        'toil==' + toil_version,
+        's3am==' + s3am_version],
+    tests_require=[
+        'pytest==2.8.3'],
+    package_dir={'': 'src'},
+    packages=find_packages('src'))
+
+
+setup(**kwargs)
+
+
+print("\n\n"
+      "Thank you for installing toil-lib! If you want to run Toil on a cluster in a cloud, please reinstall it "
+      "with the appropriate extras. For example, To install AWS/EC2 support for example, run "
+      "\n\n"
+      "pip install toil[aws,mesos]==%s"
+      "\n\n"
+      "on every EC2 instance. Refer to Toil's documentation at http://toil.readthedocs.io/en/latest/installation.html "
+      "for more information."
+      % toil_version)
