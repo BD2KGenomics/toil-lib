bcee8b888c2dea66763b3d2972e571e90398fa28
John Vivian <jtvivian@gmail.com>
2016-09-26 12:20:10 -0700

    Add CKCC BAM QC (resolves #40)

--- src/toil_lib/tools/QC.py
+++ src/toil_lib/tools/QC.py
@@ -27,3 +27,33 @@ def run_fastqc(job, r1_id, r2_id):
     output_files = [os.path.join(work_dir, x) for x in output_names]
     tarball_files(tar_name='fastqc.tar.gz', file_paths=output_files, output_dir=work_dir)
     return job.fileStore.writeGlobalFile(os.path.join(work_dir, 'fastqc.tar.gz'))
+
+
+def run_bam_qc(job, aligned_bam_id, uuid=None):
+    """
+    Run BAM QC as specified by California Kids Cancer Comparison (CKCC)
+
+    :param JobFunctionWrappingJob job:
+    :param str aligned_bam_id: FileStoreID of sorted bam from STAR
+    :param None|str uuid: Will prefix to output names if set
+    :return: boolean flag, FileStoreID for output bam, and FileStoreID for output tar
+    :rtype: tuple(bool, str, str)
+    """
+    work_dir = job.fileStore.getLocalTempDir()
+    job.fileStore.readGlobalFile(aligned_bam_id, os.path.join(work_dir, 'rnaAligned.sortedByCoord.out.bam'))
+    docker_call(tool='hbeale/treehouse_bam_qc:1.0', work_dir=work_dir, parameters=['runQC.sh', str(job.cores)])
+
+    # Tar Output files
+    output_names = ['readDist.txt', 'rnaAligned.out.md.sorted.geneBodyCoverage.curves.pdf',
+                    'rnaAligned.out.md.sorted.geneBodyCoverage.txt']
+    output_files = [os.path.join(work_dir, x) for x in output_names]
+    prefix = '' if None else uuid + '.'
+    tar_path = tarball_files(tar_name='bam_qc.tar.gz', file_paths=output_files, output_dir=work_dir, prefix=prefix)
+
+    # Save output BAM
+    sorted_bam_id = job.fileStore.writeGlobalFile(os.path.join(work_dir, 'rnaAligned.sortedByCoord.md.bam'))
+
+    # Check for FAIL flag
+    fail_flag = True if os.path.exists(os.path.join(work_dir, 'readDist.txt_FAIL_qc.txt')) else False
+
+    return fail_flag, sorted_bam_id, job.fileStore.writeGlobalFile(tar_path)
