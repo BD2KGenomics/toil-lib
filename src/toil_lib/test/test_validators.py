4ff5392072673a680c0e37f432443092cacb22b9
CJ <cketchum@ucsc.edu>
2016-10-05 13:09:16 -0700

    Update quick check bam test with new url methods

--- src/toil_lib/test/test_validators.py
+++ src/toil_lib/test/test_validators.py
@@ -1,14 +1,19 @@
 import os
-
 from toil_lib.urls import download_url
+from toil_lib.test import DockerCallTest
+from toil.job import Job
+from toil.common import Toil
 
-
-def test_bam_quickcheck(tmpdir):
-    from toil_lib.validators import bam_quickcheck
-    work_dir = str(tmpdir)
-    good_bam_url = 's3://cgl-pipeline-inputs/exome/ci/chr6.normal.bam'
-    bad_bam_url = 's3://cgl-pipeline-inputs/exome/ci/truncated.bam'
-    download_url(url=good_bam_url, name='good.bam', work_dir=work_dir)
-    download_url(url=bad_bam_url, name='bad.bam', work_dir=work_dir)
-    assert bam_quickcheck(os.path.join(work_dir, 'good.bam'))
-    assert bam_quickcheck(os.path.join(work_dir, 'bad.bam')) == False
+class TestValidators(DockerCallTest):
+    def test_bam_quickcheck(self):
+        from toil_lib.validators import bam_quickcheck
+        good_bam_url = 's3://cgl-pipeline-inputs/exome/ci/chr6.normal.bam'
+        bad_bam_url = 's3://cgl-pipeline-inputs/exome/ci/truncated.bam'
+        good_bam_job = Job.wrapJobFn(download_url, url=good_bam_url, name='good.bam', work_dir=self.tmpdir)
+        with Toil(self.options) as toil:
+            toil.start(good_bam_job)
+        bad_bam_job = Job.wrapJobFn(download_url, url=bad_bam_url, name='bad.bam', work_dir=self.tmpdir)
+        with Toil(self.options) as toil:
+            toil.start(bad_bam_job)
+        assert bam_quickcheck(os.path.join(self.tmpdir, 'good.bam'))
+        assert bam_quickcheck(os.path.join(self.tmpdir, 'bad.bam')) == False
